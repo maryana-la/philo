bddf3c4833e34784c0455eb77fe125f917d013dd
maryana-la <m.layshchuk@gmail.com>
2021-07-11 00:15:26 +0300

    threads per phillosoper added

--- main.c
+++ main.c
@@ -28,58 +28,121 @@ int	check_args_valid(int argc, char **argv)
 			}
 		}
 	}
+
 	return (0);
 }
 
-void	test()
+void init_structure(t_all *all, int argc, char **argv)
 {
-	int i;
+	all->number_of_philo = ft_atoi(argv[1]);
+	all->time_to_die = ft_atoi(argv[2]) * 1000;
+	all->time_to_eat = ft_atoi(argv[3]) * 1000;
+	all->time_to_sleep = ft_atoi(argv[4]) * 1000;
+	if (argc == 6)
+		all->num_of_meal = ft_atoi(argv[5]);
+	else
+		all->num_of_meal = -1;
 
-	for (i = 0; i < 10; i++)
+	if (all->number_of_philo < 1 || all->time_to_die < 1 || all->time_to_eat < 1 || all->time_to_sleep < 1 || all->num_of_meal == 0)
 	{
-		printf("test_func %d\n", i);
-		usleep(5);
+		write(1, "Argument is too small\n", 22);
+		exit (-1);
 	}
+	gettimeofday(&all->start, NULL);
+	all->start_time = (all->start.tv_sec * 1000) + (all->start.tv_usec / 1000);
+}
+
+void *routine(void *i)
+{
+	t_philo	*ph;
+
+
+	ph = (t_philo *)i;
+	while (1)
+	{
+//eating
+		pthread_mutex_lock(ph->left_fork);
+		gettimeofday(&ph->all->end, NULL);
+		printf("%ld ms %d has taken a left fork\n", (((ph->all->end.tv_sec * 1000) + (ph->all->end.tv_usec / 1000)) - ph->all->start_time), ph->num);
+		pthread_mutex_lock(ph->right_fork);
+		gettimeofday(&ph->all->end, NULL);
+		printf("%ld ms %d has taken a right fork\n", (((ph->all->end.tv_sec * 1000) + (ph->all->end.tv_usec / 1000)) - ph->all->start_time),  ph->num);
+		printf("%ld ms %d is eating\n", (((ph->all->end.tv_sec * 1000) + (ph->all->end.tv_usec / 1000)) - ph->all->start_time), ph->num);
+		usleep(ph->all->time_to_eat);
+		pthread_mutex_unlock(ph->left_fork);
+		pthread_mutex_unlock(ph->right_fork);
+
+//	sleeping
+		gettimeofday(&ph->all->end, NULL);
+		printf("%ld ms %d is sleeping\n", (((ph->all->end.tv_sec * 1000) + (ph->all->end.tv_usec / 1000)) - ph->all->start_time), ph->num);
+		usleep(ph->all->time_to_sleep);
+
+//thinking
+		gettimeofday(&ph->all->end, NULL);
+		printf("%ld ms %d is thinking\n", (((ph->all->end.tv_sec * 1000) + (ph->all->end.tv_usec / 1000)) - ph->all->start_time), ph->num);
+		usleep(100);
+	}
+	return (0);
 }
 
 int main (int argc, char **argv)
 {
-	t_philo philo;
-	struct timeval start;
-	struct timeval end;
-	struct timezone tz;
+	t_all all;
+	int		i;
 
-	gettimeofday(&start, &tz);
+
+
 	if (check_args_valid(argc, argv) < 0)
 		exit (-1);
+	init_structure(&all, argc, argv);
 
-	//todo check for min max value
+	// forks create
+	all.forks = malloc(sizeof(pthread_mutex_t) * all.number_of_philo);
+	i = -1;
+	while(++i < all.number_of_philo)
+		pthread_mutex_init(&all.forks[i], NULL);
 
-	philo.number_of_philo = ft_atoi(argv[1]);
-	if (philo.number_of_philo < 1)
+	// philosophers init
+	t_philo *philo;
+	i = -1;
+	philo = malloc(sizeof(t_philo) * all.number_of_philo);
+	while(++i < all.number_of_philo)
 	{
-		write(1, "Wrong number of philosophers\n", 29);
-		return (-1);
+		philo[i].num = i + 1;
+		philo[i].last_ate = 0;
+		philo[i].num_eat = 0;
+		philo[i].all = &all;
+		if ((i + 1) != all.number_of_philo)
+		{
+			philo[i].left_fork = &all.forks[i];
+			philo[i].right_fork = &all.forks[i + 1];
+		}
+		else
+		{
+			philo[i].left_fork = &all.forks[i];
+			philo[i].right_fork = &all.forks[0];
+		}
 	}
-	philo.time_to_die = ft_atoi(argv[2]);
-	philo.time_to_eat = ft_atoi(argv[3]);
-	philo.time_to_sleep = ft_atoi(argv[4]);
-	if (argc == 6)
-		philo.num_of_meal = ft_atoi(argv[5]);
-	else
-		philo.num_of_meal = -1;
 
-	pthread_t p1;
-	pthread_t p2;
-	pthread_create(&p1, NULL, (void *)test, NULL);
-	pthread_create(&p2, NULL, (void *)test, NULL);
-	pthread_join(p1, NULL);
-	pthread_join(p2, NULL);
+	// threads create
+	pthread_t	*ph;
+	ph = malloc(sizeof(pthread_t) * all.number_of_philo);
+	i = -1;
+	gettimeofday(&all.start, NULL);
+	while(++i < all.number_of_philo)
+	{
+		pthread_create(&ph[i], NULL, &routine, (void *)&philo[i]);
+		usleep(10);
+	}
+
+	// threads close
+	i = -1;
+	while(++i < all.number_of_philo)
+		pthread_join(ph[i], NULL);
 
 
-	gettimeofday(&end, &tz);
-	printf("Time to execute program %ld microseconds\n", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
-	return (0);
+	gettimeofday(&all.end, NULL);
+	printf("Time to execute program %ld milliseconds\n", (((all.end.tv_sec * 1000) + (all.end.tv_usec / 1000)) - ((all.start.tv_sec * 1000) + (all.start.tv_usec / 1000))));
 }
 
 
--- philo.h
+++ philo.h
@@ -12,14 +12,30 @@
 # include <sys/time.h>
 # include <time.h>
 
-typedef struct	s_philo
+typedef struct	s_all
 {
-	int	number_of_philo;
-	int time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int num_of_meal;
+	int			number_of_philo;
+	long int	start_time;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			num_of_meal;
+	struct timeval start;
+	struct timeval end;
+	pthread_mutex_t	*forks;
+}				t_all;
+
+typedef struct s_philo
+{
+	int 		last_ate;
+	int 		num_eat;
+	int 		num;
+	struct s_all	*all;
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t *right_fork;
 }				t_philo;
+
+
 
 int	check_args_valid(int argc, char **argv);
 
